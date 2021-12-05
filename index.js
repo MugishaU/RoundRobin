c6392ef10f1d822b4ac29cdb018289d9b5fe8487
MugishaU <muwiragiye123@gmail.com>
2021-12-05 23:45:46 +0000

    add aws-sdk

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
+const AWS = require("aws-sdk")
+
 let testGiving = [
 	{ name: "Test 1", email: "mugisha.au+test1@gmail.com" },
 	{ name: "Test 2", email: "mugisha.au+test2@gmail.com" },
 	{ name: "Test 3", email: "mugisha.au+test3@gmail.com" },
 	{ name: "Test 4", email: "mugisha.au+test4@gmail.com" },
 	{ name: "Test 5", email: "mugisha.au+test5@gmail.com" },
-];
+]
 
 function RoundRobin(list) {
-	let people = list;
-	let firstPerson;
-	let giver;
+	let people = list
+	let firstPerson
+	let giver
+	let result = []
 
 	if (people.length > 1) {
 		while (people.length > 0) {
 			if (giver === undefined) {
-				const firstGiver = pickAndRemove(people);
+				const firstGiver = pickAndRemove(people)
 
-				giver = firstGiver;
-				firstPerson = firstGiver;
+				giver = firstGiver
+				firstPerson = firstGiver
 			}
-			const reciever = pickAndRemove(people);
+			const reciever = pickAndRemove(people)
 
-			EmailSend(giver, reciever);
-			giver = reciever;
+			result.push(EmailSend(giver, reciever))
+			giver = reciever
 		}
-		EmailSend(giver, firstPerson);
+		result.push(EmailSend(giver, firstPerson))
 	}
+	return result
 }
 
 function pickAndRemove(list) {
-	const listIndex = Math.floor(Math.random() * list.length);
-	const listItem = list.splice(listIndex, 1)[0];
-	return listItem;
+	const listIndex = Math.floor(Math.random() * list.length)
+	const listItem = list.splice(listIndex, 1)[0]
+	return listItem
 }
 
 function EmailSend(giver, reciever) {
-	console.log(`Giver: ${giver.name}, Reciever: ${reciever.name}!`);
+	return `Giver: ${giver.name}, Reciever: ${reciever.name}!`
 }
 
-RoundRobin(testGiving);
+console.log(RoundRobin(testGiving))
