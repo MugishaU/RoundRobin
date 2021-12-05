1be0a3c71f25edfee0125f6ecc4ae19b25bb7889
MugishaU <muwiragiye123@gmail.com>
2021-12-05 22:41:16 +0000

    move list splice into function

--- roundRobin.js
+++ roundRobin.js
@@ -4,33 +4,34 @@ let testGiving = [
 	{ name: "Test 3", email: "mugisha.au+test3@gmail.com" },
 	{ name: "Test 4", email: "mugisha.au+test4@gmail.com" },
 	{ name: "Test 5", email: "mugisha.au+test5@gmail.com" },
-	{ name: "Test 6", email: "mugisha.au+test6@gmail.com" },
-	{ name: "Test 7", email: "mugisha.au+test6@gmail.com" },
-	{ name: "Test 8", email: "mugisha.au+test6@gmail.com" },
-	{ name: "Test 9", email: "mugisha.au+test6@gmail.com" },
 ];
 
-function RoundRobin(people) {
+function RoundRobin(list) {
+	let people = list;
 	let firstPerson;
 	let giver;
 
 	if (people.length > 1) {
 		while (people.length > 0) {
 			if (giver === undefined) {
-				const firstGiverIndex = Math.floor(Math.random() * people.length);
-				const firstGiver = people.splice(firstGiverIndex, 1)[0];
+				const firstGiver = pickAndRemove(people);
+
 				giver = firstGiver;
 				firstPerson = firstGiver;
 			}
-
-			const recieverIndex = Math.floor(Math.random() * people.length);
-			const reciever = people.splice(recieverIndex, 1)[0];
+			const reciever = pickAndRemove(people);
 
 			EmailSend(giver, reciever);
 			giver = reciever;
 		}
 		EmailSend(giver, firstPerson);
 	}
+}
+
+function pickAndRemove(list) {
+	const listIndex = Math.floor(Math.random() * list.length);
+	const listItem = list.splice(listIndex, 1)[0];
+	return listItem;
 }
 
 function EmailSend(giver, reciever) {
